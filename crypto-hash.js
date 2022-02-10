a85bc796f4df06aa3303e55ce6568d70f9005e3e
Luis Inacio <linacio@deloitte.pt>
2022-02-10 01:08:58 +0000

    hex to binary fix

--- crypto-hash.js
+++ crypto-hash.js
@@ -1,12 +1,11 @@
 const crypto = require("crypto");
-const hexToBinary = require("hex-to-binary");
 
 const cryptoHash = (...args) => {
     const hash = crypto.createHash("sha256");
 
     hash.update(args.sort().join(" "));
 
-    return hexToBinary(hash.digest("hex"));
+    return hash.digest("hex");
 };
 
 module.exports = cryptoHash;
