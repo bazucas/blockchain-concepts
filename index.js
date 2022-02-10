b5682f8a9649af4fed20c5c7e8e1272cd7ee4315
Luis Inacio <linacio@deloitte.pt>
2022-02-10 21:03:49 +0000

    pubsub fix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const bodyParser = require("body-parser");
 const express = require("express");
+const request = require("request");
 const Blockchain = require("./blockchain");
 const PubSub =  require("./pubsub");
 
@@ -7,7 +8,11 @@ const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
 
-setTimeout(() => pubsub.broadcastChain(), 1000);
+
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
+
+// setTimeout(() => pubsub.broadcastChain(), 1000);
 
 app.use(bodyParser.json());
 
@@ -25,7 +30,18 @@ app.post("/api/mine", (req, res) => {
     res.redirect("/api/blocks");
 });
 
-const DEFAULT_PORT = 3000;
+const syncChains = () => {
+    request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, () => {
+        (error, response, body) => {
+            if (!error && response.statusCode === "200") {
+                const rootChain = JSON.parse(body);
+                console.log("replace chain on sync with", rootChain);
+                blockchain.replaceChain(rootChain);
+            }
+        }
+    })
+}
+
 let PEER_PORT;
 
 if(process.env.GENERATE_PEER_PORT === "true") {
@@ -35,4 +51,8 @@ if(process.env.GENERATE_PEER_PORT === "true") {
 const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
     console.log(`Listening at localhost:${PORT}`)
+
+    if (PORT !== DEFAULT_PORT) {
+        syncChains();
+    } 
 });
